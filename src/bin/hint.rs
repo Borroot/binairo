e0e8a23cc45686d6b6d254422d8923ae4dba99cd
borroot <borroot@live.nl>
2022-04-19 16:48:41 +0200

    Add a hint binary

---
+++ src/bin/hint.rs
@@ -0,0 +1,20 @@
+use binairo::{analyzer::tactics::{self, Tactic}, puzzle};
+
+fn main() {
+    let puzzle = puzzle::Puzzle::from_codex("a1b01a1b10a0b01101010", 6, 4).unwrap();
+    let tactics = vec![
+        tactics::Tactics::Row2,
+        tactics::Tactics::Row3,
+        tactics::Tactics::CountFixed,
+        tactics::Tactics::CountGuess,
+        tactics::Tactics::Uniqueness,
+    ];
+
+    for tactic in tactics {
+        let hints = tactic.hints(&puzzle);
+        if hints.len() > 0 {
+            println!("{:?} {:?}", tactic, hints);
+            break;
+        }
+    }
+}

45c7ce993cd6cba8ef25e071fa2c5a8f0a878caa
borroot <inbox@borroot.com>
2022-08-23 15:34:36 +0200

    Add size to bulk output

--- src/bin/bulk.rs
+++ src/bin/bulk.rs
@@ -2,17 +2,20 @@ use binairo::{analyzer::level, generator, solver};
 
 fn main() {
     for _ in 0..45 {
-        let gen = generator::gen(12, 12, level::Level::Easy, None).unwrap();
-        println!("{} {}", gen.codex(), solver::solve(&gen).unwrap().codex());
+        let size = 12;
+        let gen = generator::gen(size, size, level::Level::Easy, None).unwrap();
+        println!("{} {} {}", size, gen.codex(), solver::solve(&gen).unwrap().codex());
     }
     println!();
     for _ in 0..14 {
-        let gen = generator::gen(8, 8, level::Level::Medium, None).unwrap();
-        println!("{} {}", gen.codex(), solver::solve(&gen).unwrap().codex());
+        let size = 8;
+        let gen = generator::gen(size, size, level::Level::Medium, None).unwrap();
+        println!("{} {} {}", size, gen.codex(), solver::solve(&gen).unwrap().codex());
     }
     println!();
     for _ in 0..1 {
-        let gen = generator::gen(8, 8, level::Level::Inhuman, None).unwrap();
-        println!("{} {}", gen.codex(), solver::solve(&gen).unwrap().codex());
+        let size = 8;
+        let gen = generator::gen(size, size, level::Level::Inhuman, None).unwrap();
+        println!("{} {} {}", size, gen.codex(), solver::solve(&gen).unwrap().codex());
     }
 }
